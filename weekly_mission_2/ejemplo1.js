1585101a815186d95c015e03ee90c31164cace12
Yakin Dario <dario.mendez@alumnos.udg.mx>
2022-06-28 23:14:42 -0500

    add: ejercicio 1 y 2

---
+++ weekly_mission_2/ejemplo1.js
@@ -0,0 +1,4 @@
+//Imprimi un elemento de la lista|
+const numbers = [1,2,3,4,5];
+console.log("Imprimir un elemento de una lista");
+numbers.forEach(num => console.log(num));
---
+++ weekly_mission_2/ejemplo2.js
@@ -0,0 +1,8 @@
+//Ejemplo 2: for Each para calcular la suma de todos de los elementos de una lista
+//
+
+let sum = 0;
+const numbers = [1,2,3,4,5];
+numbers.forEach(num => sum += num)
+console.log("Ejemplo 2: Calcular de la suma de todos de los elementos de la lista");
+console.log(sum);
