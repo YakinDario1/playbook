01f968cda1d580ade4c150a73bbdf7cc681cfd42
Yakin Dario <dario.mendez@alumnos.udg.mx>
2022-07-06 19:45:34 -0500

    add: ejemplo 9

---
+++ weekly_mission_2/ejemplo9.js
@@ -0,0 +1,7 @@
+//  Ejemplo 9: Uso de Reduce
+const number = [1, 2, 3, 4, 5];
+const result = number.reduce((acc, element) => acc + element, 0);
+console.log(
+    "Ejemplo 9: Uso de reduce para calcular la suma de los elementos de una lista"
+);
+console.log(result);
